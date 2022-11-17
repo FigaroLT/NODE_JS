7f8043811f984109b5c5201106bc40d343134ed1
figaroltgit config --global user.email logrez@gmail.comgit add .git statuscd vgzgit config --global user.name figaroltgit config --global user.name figarolt <logrez@gmail.com>
2022-11-17 22:35:30 +0200

    NodeJS relocated

---
+++ Node pradmenys/server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const port = 5_000;
+const randomNumber = Math.round(Math.random());
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+
+  if (randomNumber) {
+    res.statusCode = 403;
+    res.end("Error! Something went wrong!");
+  }
+  res.statusCode = 200;
+  res.end("Hello!!!");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running ${randomNumber}.`);
+});
